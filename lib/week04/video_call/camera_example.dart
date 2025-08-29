aba9a56db44142b75ec33b896058ab0769e570d6
Jung-Been Lee <jungbini@gmail.com>
2025-08-29 22:07:06 +0900

    Fix: Add missing `main` function in camera example.

--- lib/week04/video_call/camera_example.dart
+++ lib/week04/video_call/camera_example.dart
@@ -1,4 +1,6 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
-late List<CameraDescription> _cameras;``
+late List<CameraDescription> _cameras;
+
+Future<void> main() 
